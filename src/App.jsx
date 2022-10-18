a7603ba7db8a1b73d8f28fc6930ca0255e670b2f
Mitja <50422993+MitjaRebo@users.noreply.github.com>
2022-10-18 21:30:05 +0200

    Addressing lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import { useState } from "react";
+import * as React from "react";
+
+const list = [
+  {
+    title: "React",
+    url: "https://react.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1> My hacker story </h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+
+      <hr />
+
+      <ul>
+        {list.map((i) => (
+          <li key={i.objectID}>
+            <span>
+              <a href={i.url}>{i.title} </a>
+            </span>
+            <span>{i.author} </span>
+            <span>{i.num_comments} </span>
+            <span>{i.points} </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
